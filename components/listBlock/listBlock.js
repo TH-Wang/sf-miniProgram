2c0e6de1c3f2716dc368f330c2bf0ecf7ed938a4
wyl <2302771786@qq.com>
2020-06-04 02:41:10 +0800

    update the structure and style of food list

---
+++ components/listBlock/listBlock.js
@@ -0,0 +1,30 @@
+// components/listBlock/listBlock.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    block: {
+      type: Object,
+      default: {}
+    },
+    mode: {
+      type: String,
+      default: "full"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
