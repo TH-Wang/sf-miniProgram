885650cf8631953dfa862d11719ee40b3282c33b
wyl <2302771786@qq.com>
2020-06-04 02:36:24 +0800

    update the globalData and add packing the list of response data

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
 //app.js
 App({
-  initFoodList(){
+  requestList(){
     var __this__ = this;
     wx.request({
       url: "http://localhost:3000/admin/food/list?storeid=10",
       method: "GET",
       success: function(res){
+        let packData = __this__.packingData(res.data.list)
+        __this__.globalData.foodList = packData.foodList;
+        __this__.globalData.menuList = packData.menuList;
         console.log(res.data);
-        __this__.globalData.foodList = res.data.list;
+        console.log(packData.foodList);
+      },
+      fail: function(err){
+        console.log(err);
       }
     })
+  },
+  packingData(data){
+    var foodList = [];
+    var menuList = [];
+    // 初始化置顶列表
+    var topping = [
+      {typename: "热销", mode: "isHot"},
+      {typename: "新品", mode: "isNew"},
+      {typename: "优惠", mode: "isSale"}
+    ]
+    // 过滤出置顶的菜品列表
+    for (const item of topping) {
+      foodList.push({
+        typename: item.typename,
+        mode: item.mode,
+        list: data.filter(el => el[item.mode])
+      })
+    }
+    // 数组去重，获取所有类别
+    for (const item of data) {
+      if(!menuList.some(el => el.typename == item.typename))
+        menuList.push({
+          typename: item.typename,
+          mode: "normal"
+        });
+    }
+    // 过滤各类别的列表
+    for (const item of menuList) {
+      foodList.push({
+        typename: item.typename,
+        mode: "normal",
+        list: data.filter(el => el.typename == item.typename)
+      })
+    }
+    // 合并类别数组
+    menuList = [...topping, ...menuList]
+    return { foodList, menuList }
   },
   getSystemInfo(){
     try {
@@ -29,21 +72,22 @@ App({
     let s = this.globalData.systemInfo.statusBarHeight;
     let m = this.globalData.systemInfo.menuButtonBounding;
     // 计算导航栏的高度
-    let height = (m.top - s) * 2 + m.height;
+    let height = (m.top - s) * 2 + m.height + 2;
     this.globalData.headerStyle = {
       totalHeight: s + height,
       headerPaddingTop: s,
       headerWidth: m.left,
-      headerHeight: height + 2
+      headerHeight: height
     }
   },
   onLaunch: function () {
-    this.initFoodList();
+    this.requestList();
     this.getSystemInfo();
     this.computedHeaderStyle();
   },
   globalData: {
     foodList: null,
+    menuList: null,
     systemInfo: null,
     headerStyle: null
   }
