294d52fd7032906e41ed5f69736fe256572d4d81
wyl <2302771786@qq.com>
2020-06-10 01:45:00 +0800

    finished: The reactive computed of sum price, preferential total and count

--- components/shopCart/shopCart.js
+++ components/shopCart/shopCart.js
@@ -14,7 +14,10 @@ Component({
    * 组件的初始数据
    */
   data: {
-    openCart: false
+    openCart: false,
+    cartCount: 0,
+    sumNum: 0,
+    saleSumNum: 0
   },
 
   /**
@@ -27,6 +30,28 @@ Component({
     handleCloseCart(e){
       if(e.target.id == "cart-mask")
         this.setData({ openCart: false })
+    },
+    handleReduceCount(e){
+      let el = e.target.dataset.item
+      this.triggerEvent("cart", {type: "reduce", data: el})
+    },
+    handleAddCount(e){
+      let el = e.target.dataset.item
+      this.triggerEvent("cart", {type: "add", data: el})
+    }
+  },
+
+  observers: {
+    "cartList": function(val){
+      let count = val.reduce((prev, item) => prev + item.count, 0)
+      let sum = val.reduce((prev, item) => prev + item.sum, 0)
+      let oriSum = val.reduce((prev, item) => prev + item.oriSum, 0)
+      let saleSum = oriSum - sum
+      this.setData({
+        cartCount: count,
+        sumNum: sum,
+        saleSumNum: saleSum
+      })
     }
   }
 })
